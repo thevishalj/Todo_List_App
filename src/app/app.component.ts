9f881db66267d73921ad22c61ac944b01f8a2581
Vishal Jain <61986769+thevishalj@users.noreply.github.com>
2023-09-23 09:05:59 +0000

    Created and mapped components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //we are calling this from index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CodeSandbox';
+  title = 'TODO List';
 }
